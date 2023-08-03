feba93eace1c37dee00401fde4dac7d5d0d8ff69
NICK NADEAU <j.nicholas.nadeau@outlook.com>
2023-08-03 12:50:01 -0700

    some testing on my end

--- handlers/user/creation.py
+++ handlers/user/creation.py
@@ -1,13 +1,16 @@
 import json
 
-
 def test(event, context):
+    message = event.get("message", "No message found")
+
     body = {
         "message": "andre and nick were here",
         "input": event,
+        "received_message": message,
     }
 
     print("please show up on cloudwatch")
+    print(message)
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
--- handlers/user/waitlist.py
+++ handlers/user/waitlist.py
@@ -1,3 +1,24 @@
+import boto3
+import json
+
+s3 = boto3.client("s3")
 
 
+def joinWaitlist(event, context):
+    print(event)
 
+    body = event["body"]
+    print(body)
+
+    email = body["email"]
+    print(email)
+
+    bucket = "triple-leads-mvp-emails"
+    key = f"{email}.json"
+
+    s3.put_object(Bucket=bucket, Key=key, Body=json.dumps(email))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(f"User {email} successfully added to the waitlist."),
+    }
